51ed54e2f21fb0e0c186b5552e2e4f3840d06818
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-07-18 18:54:43 +0300

    Create miniclock.js and add displayInRussian() function there

---
+++ miniclock.js
@@ -0,0 +1,34 @@
+// Displays Russian version of date and time in all elements with .miniclock class.
+function displayInRussian() {
+	const weekDayStrings = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+	const monthStrings = [
+		"января",
+		"февраля",
+		"марта",
+		"апреля",
+		"мая",
+		"июня",
+		"июля",
+		"августа",
+		"сентября",
+		"октября",
+		"ноября",
+		"декабря"
+	];
+	let now = new Date();
+	let text =
+		"Сегодня " +
+		weekDayStrings[now.getDay()] +
+		", " +
+		now.getDate() +
+		" " +
+		monthStrings[now.getMonth()] +
+		" " +
+		now.getFullYear() +
+		" года, " +
+		now.toLocaleTimeString(new Intl.Locale("ru"));
+	let targets = document.getElementsByClassName("miniclock");
+	for (let target of targets) {
+		target.innerText = text;
+	}
+}
