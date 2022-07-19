00432829fc59c68d8e7f22298729b552bdb9ea0d
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-07-19 12:01:39 +0300

    Enclose all functions into `miniclock` object to prevent name conflicts

--- miniclock.js
+++ miniclock.js
@@ -1,7 +1,13 @@
 // © Igor Gorlov, 2022. This work is protected by BSD 3-clause license.
 
+/*
+To prevent name conflicts, all Miniclock functions (defined below) reside in a global object,
+which works as a namespace.
+*/
+let miniclock = {};
+
 // Displays Russian version of date and time in all elements with .miniclock class.
-function displayInRussian() {
+miniclock.displayInRussian = () => {
 	const weekDayStrings = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
 	const monthStrings = [
 		"января",
@@ -33,15 +39,15 @@ function displayInRussian() {
 	for (let target of targets) {
 		target.innerText = text;
 	}
-}
+};
 
 // Detects end-user's preferred language and calls the appropriate printing function.
-function start() {
+miniclock.start = () => {
 	if (navigator.language.startsWith("ru")) {
-		displayInRussian();
-		setInterval(displayInRussian, 1000);
+		miniclock.displayInRussian();
+		setInterval(miniclock.displayInRussian, 1000);
 	}
-}
+};
 
 // Start the whole system when DOM is ready.
-document.addEventListener("DOMContentLoaded", start);
+document.addEventListener("DOMContentLoaded", miniclock.start);
