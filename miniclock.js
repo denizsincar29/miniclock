53a11fe63b5abdbb9a98896fc2040925281b16fa
denizsincar29 <bomberman29ds@yandex.ru>
2022-11-17 10:54:39 +0300

    screenreader improovements

--- miniclock.js
+++ miniclock.js
@@ -20,20 +20,42 @@ miniclock.display = () => {
 		.replace("%lt",now.toLocaleTimeString(new Intl.Locale(miniclock.language)));
 	let targets = document.getElementsByClassName("miniclock");
 	for (let target of targets) {
-		target.innerText = text;
+		target.children[1].setAttribute("aria-label",text);
+		target.children[0].innerText = text;
 	}
 };
 
-miniclock.initData=function(){
+miniclock.initData=() => {
 	load_json("./locale/"+miniclock.language+".json")
 		.then(miniclock.start);
 }
 
 // Detects end-user's preferred language and calls the appropriate printing function.
 miniclock.start = () => {
+	// prepare the miniclock elements to improove screenreader accessibility
+	let targets = document.getElementsByClassName("miniclock");
+	for (let target of targets) {
+		// make 2 divs inside the miniclock class div.
+		// one is a div that will only be read by screenreader and hidden on the screen,
+		// and the second is a div that will be hidden for screenreaders but visible on the screen
+		let real_div = document.createElement("div");
+		let sr_div = document.createElement("div");
+		real_div.setAttribute("aria-hidden","true");
+		sr_div.setAttribute("aria-label","miniclock area");
+		sr_div.setAttribute("tabindex","0");
+		sr_div.classList.add("visually-hidden");
+		target.appendChild(real_div); target.appendChild(sr_div);
+
+
+	}
+
+
 	miniclock.display();
 	setInterval(miniclock.display, 1000);
-
+	if(typeof draw_clockface==="function"){
+		// if we imported a <script src="clockface.js">, we call the function draw clockface
+		draw_clockface();
+	}
 };
 
 // Start the whole system when DOM is ready.
